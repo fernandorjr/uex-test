37ea3a363ad4d8b80d5c9759586d7e5e64890118
Fernando Rodrigues <fernandorjr94@gmail.com>
2025-05-17 19:28:52 -0300

    feat: create auth layout

---
+++ src/components/layout/auth/auth.layout.component.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+import "./auth.layout.style.css"
+
+const AuthLayout = () => {
+  return (
+    <div className="auth-layout-container">
+      <Outlet />
+      <span className="auth-layout-author">Fernando Rodrigues</span>
+    </div>
+  )
+}
+
+export default AuthLayout
---
+++ src/components/layout/index.ts
@@ -0,0 +1,1 @@
+export {default as AuthLayout} from "./auth/auth.layout.component"
