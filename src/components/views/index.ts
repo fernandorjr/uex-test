e04b4c6af8338db56efd9e990863b034680f8859
Fernando Rodrigues <fernandorjr94@gmail.com>
2025-05-17 20:37:06 -0300

    feat: update component export method in views

---
+++ src/components/views/index.ts
@@ -0,0 +1,2 @@
+export { default as LoginView } from './login/login.view.component'
+export { default as RegisterView } from './register/register.page.component'
--- src/pages/auth/login.page.tsx
+++ src/pages/auth/login.page.tsx
@@ -1,4 +1,4 @@
-import LoginView from "@/components/views/login/login.view.component"
+import { LoginView } from "@/components/views"
 
 const LoginPage = () => {
   return (
