b667e9be5976cc622fb2206d38b98268fd2a9932
Fernando Rodrigues <fernandorjr94@gmail.com>
2025-05-18 03:30:07 -0300

    feat: create profile page

--- src/pages/app/index.ts
+++ src/pages/app/index.ts
@@ -1,1 +1,2 @@
 export { default as DashboardPage } from './dashboard.page'
+export { default as ProfilePage } from './profile.page'
--- src/router/router-app.tsx
+++ src/router/router-app.tsx
@@ -1,15 +1,14 @@
 import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import { ERoutes } from '../tokens/routes'
 import { AppLayout, AuthLayout } from '@/components/layout'
-import { DashboardPage } from '@/pages/app'
+import { DashboardPage, ProfilePage } from '@/pages/app'
 import { LoginPage, RecoveryPasswordPage, RegisterPage } from '@/pages/auth'
-
 
 const RouterApp = () => {
   return (
     <Router>
       <Routes>
-        <Route path='*' element={<Navigate to={ERoutes.LOGIN} />} />
+        <Route path="*" element={<Navigate to={ERoutes.LOGIN} />} />
 
         <Route element={<AuthLayout />}>
           <Route path={ERoutes.LOGIN} element={<LoginPage />} />
@@ -19,6 +18,7 @@ const RouterApp = () => {
 
         <Route element={<AppLayout />}>
           <Route path={ERoutes.DASHBOARD} element={<DashboardPage />} />
+          <Route path={ERoutes.PROFILE} element={<ProfilePage />} />
         </Route>
       </Routes>
     </Router>
